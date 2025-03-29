8dccf711b7be1cfaaaf78c314459e684536ec17b
Jakub Kuc <jakub.kuc.11@gmail.com>
2025-03-29 13:15:59 +0100

    Changed the urls and the Landing Page also clean up mess with media files

--- CarForum/settings.py
+++ CarForum/settings.py
@@ -120,8 +120,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIA_ROOT = BASE_DIR / "car_images"
-
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
